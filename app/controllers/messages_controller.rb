d358021915af64e84f20e758227a0bbb43e2f2f8
宮田昌平 <heyheyshohey0104@gmail.com>
2018-07-13 20:46:02 +0900

    メッセージ送信機能の実装　途中
    メッセージ送信機能の実装　途中です
    ４章の問題３までやりました。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,15 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def create
+    @message = Message.new(message_params)
+    redirect root_path
+  end
+
+  private
+  def message_params
+    params.permit(:image, :body)
+  end
+
 end
