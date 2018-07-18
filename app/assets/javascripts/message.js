e18200312b9ade68976ceafc936a14e1915f8386
宮田昌平 <heyheyshohey0104@gmail.com>
2018-07-18 14:59:27 +0900

    respond_to html, json
    respond_toでHTMLとJSONで処理を分けました。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -12,22 +12,3 @@ $(function() {
     })
   });
 });
-
-// message = $('.form_textbox').val();
-// console.log(message);
-
-
-// $(function(){
-//   $('#new_comment').on('submit', function(e){
-//     e.preventDefault();
-//     var formData = new FormData(this);
-//     var url = $(this).attr('action')
-//     $.ajax({
-//       url: url,
-//       type: "POST",
-//       data: formData,
-//       dataType: 'json',
-      // processData: false,
-      // contentType: false
-//     })
-//   })
