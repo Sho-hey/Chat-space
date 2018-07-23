59b3644df8a21651e554e0df07ae14a3eee3c00d
宮田昌平 <heyheyshohey0104@gmail.com>
2018-07-23 16:22:32 +0900

    add_to search-without-current_user
    検索の際にカレントユーザーが検索結果にでないようにしました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
     respond_to do |format|
       format.html{redirect_to edit_group_path}
       format.json
@@ -23,5 +23,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email)
   end
-  
+
 end
